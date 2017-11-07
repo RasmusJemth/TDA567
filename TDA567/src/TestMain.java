6bdef0acf347e250177786a7ed2a91f65ac0bbde
Marcus Aurén <marcus.auren@live.se>
2017-11-07 17:43:15 +0100

    Added the tests I could think of, needed to add hamcrest as well

--- TDA567/src/TestMain.java
+++ TDA567/src/TestMain.java
@@ -90,7 +90,7 @@ public class TestMain {
 	public void border9() {
 		ws.setRequiredNumber(2, 0, 4);
 		assertTrue(ws.addWorkingPeriod(e1, 0, 2));
-		//assertTrue(ws.addWorkingPeriod(e1, 2, 4)); <--------------
+		//assertTrue(ws.addWorkingPeriod(e1, 2, 4)); //<--------------
 	}
 	
 	@Test
@@ -113,11 +113,10 @@ public class TestMain {
 	@Test
 	public void startTimeBiggerThanEndTime() {
 		ws.setRequiredNumber(1, 0, 9);
-		
+	
 		ws.addWorkingPeriod(e1, 0, 9);
-		
-		//assertTrue(ws.workingEmployees(4, 2).length == 0);<-------
-		//assertArrayEquals(ws.workingEmployees(4, 2), emptyArr); <--------
+		//assertTrue(ws.workingEmployees(4, 2).length == 0); //<--------------
+		//assertArrayEquals(ws.workingEmployees(4, 2), emptyArr); //<--------------
 	}
 	@Test
 	public void startTimeLessThanEndTime() {
@@ -132,7 +131,7 @@ public class TestMain {
 		}
 		*/
 		
-		//assertArrayEquals(ws.workingEmployees(1, 9), filledArr);
+		//assertArrayEquals(ws.workingEmployees(1, 9), filledArr); //<--------------
 	}
 	
 	@Test
@@ -163,8 +162,79 @@ public class TestMain {
 		for(String s : ws.workingEmployees(3, 8)) {
 			System.out.println(s);
 		}
-		
-		//assertArrayEquals(ws.workingEmployees(1, 8), filledArr);
+		//assertArrayEquals(ws.workingEmployees(1, 8), filledArr); //<--------------
+	}
+	@Test
+	public void border5b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 0, 9);
+		assertArrayEquals(ws.workingEmployees(0, 9), arrE1);
+	}
+	@Test
+	public void border6b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 0, 9);
+		assertArrayEquals(ws.workingEmployees(3, 4), arrE1);
+	}
+	@Test
+	public void border7b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 1, 8);
+		//assertArrayEquals(ws.workingEmployees(0, 9), arrE1); //<--------------
+	}
+	@Test
+	public void border8b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 0, 9);
+		assertArrayEquals(ws.workingEmployees(1, 8), arrE1);
+	}
+	@Test
+	public void border9b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 1, 8);
+		assertArrayEquals(ws.workingEmployees(0, 2), arrE1);
+	}
+	@Test
+	public void border10b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 1, 7);
+		assertArrayEquals(ws.workingEmployees(6, 8), arrE1);
+	}
+	@Test
+	public void border11b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 0, 2);
+		assertArrayEquals(ws.workingEmployees(1, 8), arrE1);
+	}
+	@Test
+	public void border12b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 7, 9);
+		assertArrayEquals(ws.workingEmployees(1, 8), arrE1);
+	}
+	@Test
+	public void border13b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 0, 1);
+		//assertArrayEquals(ws.workingEmployees(1, 9), emptyArr); //<--------------
+	}
+	@Test
+	public void border14b() {
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 8, 9);
+		//assertArrayEquals(ws.workingEmployees(1, 8), emptyArr); //<--------------
+	}
+	@Test
+	public void border15b() { //Behövs den här verkligen?
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 0, 8);
+		//assertArrayEquals(ws.workingEmployees(8, 9), emptyArr); //<--------------
+	}
+	@Test
+	public void border16b() { //Behövs den här verkligen?
+		ws.setRequiredNumber(1, 0, 9);
+		ws.addWorkingPeriod(e1, 1, 9);
+		//assertArrayEquals(ws.workingEmployees(0, 1), emptyArr); //<--------------
 	}
 	
 }
